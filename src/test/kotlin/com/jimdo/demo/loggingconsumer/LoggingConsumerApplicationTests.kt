c1523d6401539cf05f253c501d1c4089c78ee15a
Hauke Stange <hauke.stange@jimdo.com>
2023-09-14 09:46:53 +0200

    Initial commit

---
+++ src/test/kotlin/com/jimdo/demo/loggingconsumer/LoggingConsumerApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.jimdo.demo.loggingconsumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoggingConsumerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
